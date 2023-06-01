cf3e110a575be841f25509cb0c54fa3eebcb4085
Justin Honda <justinohonda@gmail.com>
2023-06-01 13:23:17 -0400

    Add public initializer for CutoutBorderConfiguration

--- Sources/SwiftScan/UI/CreditCardScanner/CutoutBorderConfiguration.swift
+++ Sources/SwiftScan/UI/CreditCardScanner/CutoutBorderConfiguration.swift
@@ -9,14 +9,33 @@ import SwiftUI
 
 /// Styles a `RoundedRectangle`
 public struct CutoutBorderConfiguration {
-    public var cornerRadius: CGFloat = 0
-    public var sharpCornerLength: CGFloat = 20
-    public var style: RoundedCornerStyle = .continuous
+    public var cornerRadius: CGFloat
+    public var sharpCornerLength: CGFloat
+    public var style: RoundedCornerStyle
     public var color: Color
-    public var lineWidth: CGFloat = 4
-    public var cornerDecoration: CornerDecoration = .noDecoration
-    public var cornerDecorationColor: Color = .clear
-    public var textDecorationPosition: TextDecorationPosition = .below
+    public var lineWidth: CGFloat
+    public var cornerDecoration: CornerDecoration
+    public var cornerDecorationColor: Color
+    public var textDecorationPosition: TextDecorationPosition
+    
+    public init(
+        cornerRadius: CGFloat = 0,
+        sharpCornerLength: CGFloat = 20,
+        style: RoundedCornerStyle = .continuous,
+        color: Color, lineWidth: CGFloat = 4,
+        cornerDecoration: CornerDecoration = .noDecoration,
+        cornerDecorationColor: Color = .clear,
+        textDecorationPosition: TextDecorationPosition = .below
+    ) {
+        self.cornerRadius = cornerRadius
+        self.sharpCornerLength = sharpCornerLength
+        self.style = style
+        self.color = color
+        self.lineWidth = lineWidth
+        self.cornerDecoration = cornerDecoration
+        self.cornerDecorationColor = cornerDecorationColor
+        self.textDecorationPosition = textDecorationPosition
+    }
     
     public static var roundedWhite: Self {
         .init(cornerRadius: 10, color: .white)
